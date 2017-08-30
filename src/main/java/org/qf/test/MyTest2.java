6446887c9ba6babdb16cc642ae59a352d3f6d350
zyyyy <1148872740@qq.com>
2017-08-30 21:14:29 +0800

    Initial commit

---
+++ src/main/java/org/qf/test/MyTest2.java
@@ -0,0 +1,23 @@
+package org.qf.test;
+
+/**
+ * Created by lenovo on 2017/8/29.
+ */
+public class MyTest2 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("args = [" + args + "]");
+        System.out.println("args = " + args);
+        for (String:
+             args) {
+            System.out.println();
+            System.out.println("args = " + args);
+            System.out.println("args = [" + args + "]");
+        }
+        for (int i = 0; i < ; i++) {
+            main(
+                    args
+            );
+        }
+    }
+}
